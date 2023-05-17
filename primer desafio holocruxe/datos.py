62afd2fc50926b44f8053e0098375b9f51113a9e
BrunoMagris <brunooem.bm@gmail.com>
2023-05-17 15:47:59 -0300

    agregue primer desafio holocruxe

---
+++ primer desafio holocruxe/datos.py
@@ -0,0 +1,123 @@
+Nombre = "Bruno Omar Exequiel"
+
+Apellido = "Magris"
+
+Nacionalidad = "Argentino"
+
+Genero = "Masculino"
+
+fecha_de_nacimiento = "20/05/2004"
+
+Educación = "Secundaria"
+
+hobies = "No hacer nada"
+
+gustos_comidas = {
+    'pizza': 10,
+    'empanadas': 10,
+    'asado': 10,
+    'milanesa': 9,
+    'helado': 9,
+    'hamburguesa': 8,
+    'choripán': 8,
+    'Ñoquis': 8,
+    'churrasco': 7,
+    'tarta': 7,
+    'tacos': 6,
+    'sushi': 6,
+    'bondiola': 6,
+    'parrillada': 6,
+    'revuelto gramajo': 5,
+    'matambre': 5,
+    'locro': 5,
+    'humita': 5,
+    'rabas': 4,
+    'mariscos': 4,
+    'pollo al disco': 4,
+    'carbonada': 4,
+    'pastel de papa': 4,
+    'ravioles': 3,
+    'fideos con tuco': 3,
+    'lomito': 3,
+    'gnocchi': 3,
+    'wok de pollo': 3,
+    'bife de chorizo': 2,
+    'provoleta': 2,
+    'chipá': 2,
+    'puchero': 2,
+    'pasta frola': 2,
+    'bagna cauda': 1,
+    'mondongo': 1,
+    'sopa paraguaya': 1,
+    'pastelitos': 1,
+    'tamales': 1,
+    'criolla': 1,
+    'fainá': 1,
+    'tamal en cazuela': 1,
+    'asado al asador': 10,
+
+}
+
+gustos_musicales = {
+    'rock': 10,
+    'pop': 10,
+    'hip hop': 9,
+    'electrónica': 9,
+    'reggaetón': 8,
+    'jazz': 8,
+    'blues': 7,
+    'metal': 7,
+    'folklore': 6,
+    'cumbia': 6,
+    'tango': 5,
+    'clásica': 5,
+    'reggae': 4,
+    'salsa': 4,
+    'country': 3,
+    'punk': 3,
+    'indie': 2,
+    'rap': 2,
+    'trap': 7,
+}
+
+gustos_deportivos = {
+    'fútbol': 10,
+    'tenis': 9,
+    'rugby': 9,
+    'basquet': 8,
+    'voley': 8,
+    'automovilismo': 7,
+    'hockey': 7,
+    'boxeo': 6,
+    'atletismo': 6,
+    'handball': 5,
+    'natación': 5,
+    'ciclismo': 4,
+    'golf': 4,
+    'patinaje': 3,
+    'surf': 3,
+    'esquí': 2,
+    'paddle': 2,
+    'karate': 1,
+}
+
+gustos_generales = {
+    'viajar': 10,
+    'ver películas': 9,
+    'leer': 9,
+    'escuchar música': 8,
+    'deportes': 8,
+    'comer': 7,
+    'bailar': 7,
+    'salir con amigos': 6,
+    'jugar videojuegos': 6,
+    'pintar': 5,
+    'escribir': 5,
+    'ir de compras': 4,
+    'acampar': 4,
+    'hacer manualidades': 3,
+    'cantar': 3,
+    'tocar un instrumento': 2,
+    'jardinería': 2,
+    'meditar': 1,
+}
---
+++ primer desafio holocruxe/desafio.py
@@ -0,0 +1,18 @@
+import openai
+import os
+from datos import *
+
+# Configure la clave de la API de OpenAI
+openai.api_key = "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"
+
+pruegunta = input()
+# Enviar una solicitud de prueba a la API de ChatGPT
+prompt = pruegunta
+response = openai.Completion.create(
+    engine="davinci",
+    prompt=prompt,
+    max_tokens=100,
+    n=1,
+    stop=None,
+    temperature=0.7,
+)
