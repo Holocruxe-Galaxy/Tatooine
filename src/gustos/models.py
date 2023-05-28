07b841e16e48e690a31703c39709499ab745ee86
Facundo Fierro <fierrof_47@hotmail.com>
2023-05-28 12:28:25 +0200

    modifique models para que sean mas legibles.

--- src/gustos/models.py
+++ src/gustos/models.py
@@ -8,21 +8,23 @@ class User(models.Model):
     apellido = models.CharField(max_length=100)
     edad = models.IntegerField()
     sexo = models.CharField(max_length=100)
-    gustos_comida = models.ManyToManyField(
-        'Comida', related_name='gustos_comida')
-    comidas_consumidas = models.ManyToManyField(
-        'Comida', related_name='comidas_consumidas')
+    comidas = models.ManyToManyField("Comida", blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Tipo_Comida(models.Model):
+    nombre = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nombre
 
 
 class Comida(models.Model):
+    fecha = models.DateField()
     nombre = models.CharField(max_length=100)
-    puntaje = models.IntegerField()
+    tipo = models.ForeignKey(Tipo_Comida, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.nombre
-    
-
-
+        return f"{self.fecha}: {self.tipo} - {self.nombre}"
