bfbcff4746fb27c6257f69e151c44e0ceb1bfc09
BrunoMagris <brunooem.bm@gmail.com>
2023-07-03 15:48:51 -0300

    modificacion models

--- src/gustos/models.py
+++ src/gustos/models.py
@@ -29,3 +29,38 @@ class Comida(models.Model):
 
     def __str__(self):
         return f"{self.fecha}: {self.tipo} - {self.nombre}"
+
+
+
+
+class tipo_comida2(models.Model):
+    id_tipo_comida = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class comida2(models.Model):
+    id_comida = models.AutoField(primary_key=True)
+    tipo = models.ForeignKey(tipo_comida2, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    comidacol = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
+class usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
+class usuario_comida(models.Model):
+    id_usuario_comida = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(usuario, on_delete=models.CASCADE)
+    comida = models.ForeignKey(comida2, on_delete=models.CASCADE)
+    fecha = models.DateField()
+
+    def __str__(self):
+        return f"{self.usuario} - {self.comida} ({self.fecha})"
