c018b64fe760d1058873d3efb34390de47e15266
Facundo Fierro <fierrof_47@hotmail.com>
2023-07-07 20:13:24 +0200

    restructured utils, improved data_utils

--- holocheff/utils/model_utils.py
+++ holocheff/utils/model_utils.py
@@ -1,4 +1,16 @@
+import data_utils
+import tensorflow as tf
 
+def make_input(data_df, label_df, num_epochs=500, shuffle=True, batch_size=32):
+    def input_function():  # inner function, this will be returned
+        # create tf.data.Dataset object with data and its label
+        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))
+        if shuffle:
+            ds = ds.shuffle(1000)
+        # split dataset into batches of 32 and repeat process for number of epochs
+        ds = ds.batch(batch_size).repeat(num_epochs)
+        return ds  # return a batch of the dataset
+    return input_function  # return a function object for use
 
 # entrenar el modelo trayendo el dataframe.csv
 def train_model():
@@ -7,3 +19,6 @@ def train_model():
 def save_model():
     pass
 # # crear checkpoint para el modelo
+
+
+
