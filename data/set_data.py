e09bf81c22e5c138f1b00f69ed33c58f34a30f32
Facundo Fierro <fierrof_47@hotmail.com>
2023-08-03 11:30:44 +0200

    updated set_data.py to include new data structure

--- data/set_data.py
+++ data/set_data.py
@@ -7,114 +7,58 @@ import pymongo
 fake = Faker()
 
 # MongoDB connection
-client = pymongo.MongoClient("mongodb://localhost:27017")
+client = pymongo.MongoClient("mongodb+srv://fierrof47:qR4didvGr1qWJ9YK@testcluster.m7ynzx9.mongodb.net/")
 db = client["holocheff_db"]
 
 # MongoDB collections
-users_collection = db["users"]
-meals_collection = db["meals"]
-foods_collection = db["foods"]
+holocheff_data_collection = db["data"]
 
-# Number of synthetic data points to generate
-num_data_points = 1000
+start_date = datetime(2022, 1, 1)
 
-breakfast_list = [{'name': f'breakfast{n}'} for n in range(1, 100)]
-lunch_list = [{'name': f'lunch{n}'} for n in range(1, 100)]
-dinner_list = [{'name': f'dinner{n}'} for n in range(1, 100)]
-snack_list = [{'name': f'snack{n}'} for n in range(1, 500)]
+breakfast_list = [f'breakfast{n}' for n in range(1, 100)]
+lunch_list = [f'lunch{n}' for n in range(1, 100)]
+dinner_list = [f'dinner{n}' for n in range(1, 100)]
+snack_list = [f'snack{n}' for n in range(1, 100)]
 
-wheather_list = ['sunny', 'cloudy', 'rainy', 'snowy']
+weather_list = ['sunny', 'cloudy', 'rainy', 'snowy']
 location_list = ['home', 'restaurant', 'work']
-# Generate synthetic user data
-def generate_users(num_users):
-    users = []
-    for _ in range(num_users):
-        name = fake.name()
-        users.append({"name": name})
-    users_collection.insert_many(users)    
-    print("Users inserted into MongoDB.")
 
-# Generate synthetic food data
-def generate_foods():
-    foods = breakfast_list + \
-            lunch_list + \
-            dinner_list + \
-            snack_list
-    foods_collection.insert_many(foods) 
-    print("Foods inserted into MongoDB.")
+data = []
+# insert data in holochef_data collection
+def insert_meals(type_list):
+    # create a switch to insert time of the day (breakfast, lunch, dinner, snack)
+    time_h = 0
+    time_m = random.randint(0, 59)
+    food = ''
+    match type_list:
+        case 'breakfasts':
+                time_h = random.randint(6, 10)
+                food = random.choice(breakfast_list)
+        case 'lunches':
+                time_h = random.randint(11, 14)
+                food = random.choice(lunch_list)
+        case 'dinners':
+                time_h = random.randint(18, 22)
+                food = random.choice(dinner_list)
+        case 'snacks':
+                time_h = random.randint(15, 17)
+                food = random.choice(snack_list)
+                
+    meal_data = {
+        'date': start_date + timedelta(days=i, hours=time_h, minutes=time_m),
+        'food': food,
+        'weather': random.choice(weather_list),
+        'location': random.choice(location_list),
+    }
+    return meal_data
 
-# Generate synthetic meal data
-# Generate synthetic meal data
-def generate_meals(num_days):
-    data = []
-    # Get all user ids
-    user_ids = [user['_id'] for user in users_collection.find({}, {'_id': 1})]
+for i in range(1, 365):
+        data.append({
+               'user_id': '00000000',
+               'meal': insert_meals('breakfasts'),
+               'output': {
+                    'predicted_meal': 'predicted_meal',
+               }
+            })      
 
-    start_date = datetime(2023, 1, 1)
-
-    # Get all food items
-    breakfast_items = list(foods_collection.find({'name': {'$regex': 'breakfast\d+'}}))
-    lunch_items = list(foods_collection.find({'name': {'$regex': 'lunch\d+'}}))
-    dinner_items = list(foods_collection.find({'name': {'$regex': 'dinner\d+'}}))
-    snack_items = list(foods_collection.find({'name': {'$regex': 'snack\d+'}}))
-
-    # for each user_id in user_ids add it to the data list
-    for user_id in user_ids:
-        # For each day since the start date and for the number of days
-        for day in range(num_days):
-            # Generate breakfast data
-            breakfast_food = random.choice(breakfast_items)
-            data.append({
-                'user_id': user_id,
-                'date': start_date + timedelta(days=day, hours=random.randint(8, 11)),
-                'food_id': breakfast_food['_id'],
-                'weather': random.choice(wheather_list),
-                'location': random.choice(location_list)
-            })
-
-            # Generate lunch data
-            lunch_food = random.choice(lunch_items)
-            data.append({
-                'user_id': user_id,
-                'date': start_date + timedelta(days=day, hours=random.randint(12, 15)),
-                'food_id': lunch_food['_id'],
-                'weather': random.choice(wheather_list),
-                'location': random.choice(location_list)
-            })
-
-            # Generate dinner data
-            dinner_food = random.choice(dinner_items)
-            data.append({
-                'user_id': user_id,
-                'date': start_date + timedelta(days=day, hours=random.randint(18, 22)),
-                'food_id': dinner_food['_id'],
-                'weather': random.choice(wheather_list),
-                'location': random.choice(location_list)
-            })
-
-            # Add between 0 and 5 snacks between 8 am and 10 pm
-            for _ in range(random.randint(0, 5)):
-                snack_food = random.choice(snack_items)
-                data.append({
-                    'user_id': user_id,
-                    'date': start_date + timedelta(days=day, hours=random.randint(8, 22)),
-                    'food_id': snack_food['_id'],
-                    'weather': random.choice(wheather_list),
-                    'location': random.choice(location_list)
-                })
-
-    # Bulk insert the data
-    meals_collection.insert_many(data)
-    print("Meals inserted into MongoDB.")
-
-# The rest of the code remains unchanged
-
-generate_users(num_data_points)
-generate_foods()
-generate_meals(365)
-
-
-
-
-
-
+holocheff_data_collection.insert_many(data)
