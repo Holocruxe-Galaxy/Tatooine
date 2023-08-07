24613f9c8ab6aa0b3a85025c181c920aae5d91ec
Facundo Fierro <fierrof_47@hotmail.com>
2023-08-07 18:29:56 +0200

    modified db to be used with http hooks

--- data/set_data.py
+++ data/set_data.py
@@ -23,59 +23,42 @@ snack_list = [f'snack{n}' for n in range(1, 100)]
 weather_list = ['sunny', 'cloudy', 'rainy', 'snowy']
 location_list = ['home', 'restaurant', 'work']
 
-
-# insert data in holochef_data collection
-data = []  
+meals = []
 for i in range(1, 365):  
-    data.append({
-        'user_id': '00000000',
-        'meal':  {
-            'date': start_date + timedelta(days=i, hours=random.randint(6,11), minutes=random.randint(0,59)),
-            'food': random.choice(breakfast_list),
-            'weather': random.choice(weather_list),
-            'location': random.choice(location_list),
-        },  
-        'output': {
-                'predicted_meal': 'predicted_meal',
-        }
+    meals.append({
+        'date': start_date + timedelta(days=i, hours=random.randint(6,11), minutes=random.randint(0,59)),
+        'food': random.choice(breakfast_list),
+        'weather': random.choice(weather_list),
+        'location': random.choice(location_list),
     })
-    data.append({
-        'user_id': '00000000',
-        'meal':  {
-            'date': start_date + timedelta(days=i, hours=random.randint(12,14), minutes=random.randint(0,59)),
-            'food': random.choice(lunch_list),
-            'weather': random.choice(weather_list),
-            'location': random.choice(location_list),
-        },  
-        'output': {
-                'predicted_meal': 'predicted_meal',
-        }
+    meals.append({
+        'date': start_date + timedelta(days=i, hours=random.randint(12,14), minutes=random.randint(0,59)),
+        'food': random.choice(lunch_list),
+        'weather': random.choice(weather_list),
+        'location': random.choice(location_list),
     })
-    data.append({
-        'user_id': '00000000',
-        'meal':  {
-            'date': start_date + timedelta(days=i, hours=random.randint(18,22), minutes=random.randint(0,59)),
-            'food': random.choice(dinner_list),
-            'weather': random.choice(weather_list),
-            'location': random.choice(location_list),
-        },  
-        'output': {
-                'predicted_meal': 'predicted_meal',
-        }
+    meals.append({
+        'date': start_date + timedelta(days=i, hours=random.randint(18,22), minutes=random.randint(0,59)),
+        'food': random.choice(dinner_list),
+        'weather': random.choice(weather_list),
+        'location': random.choice(location_list),
     })
-    data.append({
-        'user_id': '00000000',
-        'meal':  {
-            'date': start_date + timedelta(days=i, hours=random.randint(6,24), minutes=random.randint(0,59)),
-            'food': random.choice(snack_list),
-            'weather': random.choice(weather_list),
-            'location': random.choice(location_list),
-        },  
-        'output': {
-                'predicted_meal': 'predicted_meal',
-        }
+    meals.append({
+        'date': start_date + timedelta(days=i, hours=random.randint(6,24), minutes=random.randint(0,59)),
+        'food': random.choice(snack_list),
+        'weather': random.choice(weather_list),
+        'location': random.choice(location_list),
     })
 
-holocheff_data_collection.insert_many(data)
+data = {
+    'user_id': '00000000',
+    'meals': meals,  
+    'output': {
+        'predicted_meal': 'predicted_meal',
+    },
+    'model': 'model_name_or_id'
+}
+
+holocheff_data_collection.insert_one(data)
 
 print('Data inserted successfully')
--- utils/model_utils.py
+++ utils/model_utils.py
@@ -2,6 +2,7 @@
 import joblib
 import os
 from bson.binary import Binary
+from io import BytesIO
 from sklearn.tree import DecisionTreeClassifier
 from utils import debug_utils as debug_utils
 from utils.data_utils import Data
@@ -35,32 +36,39 @@ class Model:
     def save(self, user_id):
         try:
             # Serialize the model to a byte stream
-            serialized_model = joblib.dump(self.model, 'temp.joblib')
-            with open('temp.joblib', 'rb') as f:
-                binary_data = Binary(f.read())
+            buffer = BytesIO()
+            joblib.dump(self.model, buffer)
+            buffer.seek(0)
+            binary_data = Binary(buffer.read())
 
             # Use existing connection method
             database = self.data.mongodb_connection()
 
             # Add a 'model' field to the user's document containing the serialized model
             database.data.update_one({"user_id": user_id}, {"$set": {"model": binary_data}})
-
-            # Remove temp file
-            os.remove('temp.joblib')
         except Exception as e:
             self.logger.error(f"Error occurred while saving: {e}", extra={'color': '91'})
             return None
 
 
+
     # Load the model from the path if it exists, else define a new model
     def load(self,user_id):
-        path = f"models/{user_id}.joblib"
-        if os.path.exists(path): # Check if the file exists
-            self.model = joblib.load(path)      
+        # Use existing connection method
+        database = self.data.mongodb_connection()
+
+        user_data = database.data.find_one({"user_id": user_id})
+
+        # Check if the model exists
+        if user_data and "model" in user_data:
+            # Load model from binary data
+            buffer = BytesIO(user_data["model"])
+            self.model = joblib.load(buffer)
         else:
+            # Define and train a new model
             self.model = self.define()      
             self.train()
-            self.save(path)
+            self.save(user_id)
         return self.model
 
     def output(self):
